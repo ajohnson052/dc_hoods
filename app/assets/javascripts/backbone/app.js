824e92efaa1546cf8688f1d2b4b36e784c4ff4d1
Alexis Johnson <alexis.johnson052@gmail.com>
2016-01-14 22:04:00 -0500

    adjust times on animation

--- app/assets/javascripts/backbone/app.js
+++ app/assets/javascripts/backbone/app.js
@@ -8,9 +8,7 @@ App = {
 $(document).ready(function(){
   new App.Routers.DCHoods;
   Backbone.history.start();
-  $("body").delay(4000).animate({
+  $("body").delay(5000).animate({
     scrollTop: $(".logo").height()
-  }, 1500)
-  // $(".logo").delay(5000).fadeOut(800);
-  // $("main").delay(5800).css("visibility", "visibile")
+  }, 500)
 });
