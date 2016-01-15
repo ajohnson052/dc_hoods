53b794370db2fac7ed2fdbd33cceeeb2b962c367
Alexis Johnson <alexis.johnson052@gmail.com>
2016-01-14 19:05:42 -0500

    animate welcome screen

--- app/assets/javascripts/backbone/app.js
+++ app/assets/javascripts/backbone/app.js
@@ -8,5 +8,9 @@ App = {
 $(document).ready(function(){
   new App.Routers.DCHoods;
   Backbone.history.start();
-  $(".logo").delay(5000).fadeOut(800);
+  $("body").delay(4000).animate({
+    scrollTop: $(".logo").height()
+  }, 1500)
+  // $(".logo").delay(5000).fadeOut(800);
+  // $("main").delay(5800).css("visibility", "visibile")
 });
