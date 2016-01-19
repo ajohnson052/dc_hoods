fc652c683da47edd980b2db95b99d751a2cbce19
Alexis Johnson <alexis.johnson052@gmail.com>
2016-01-19 16:52:31 -0500

    replace all http with https

--- app/assets/javascripts/backbone/views/hoods.js
+++ app/assets/javascripts/backbone/views/hoods.js
@@ -16,7 +16,7 @@ App.Views.Hoods = Backbone.View.extend({
   createMap: function(){
     this.map = L.map('map').setView([38.906, -77.012], 12);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      attribution: 'Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://mapbox.com">Mapbox</a>',
       maxZoom: 18,
       id: 'ajohnson052.oo8808h2',
       accessToken: 'pk.eyJ1IjoiYWpvaG5zb24wNTIiLCJhIjoiY2lqMjl3aGZqMDAwNnVia3BvbzZndTVxcSJ9.lgDF359V9thO7NOh3x_GEQ'
